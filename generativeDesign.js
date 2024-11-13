c1f563945aeeaf50e96fc429938205413caac30b
AagjeDeLeeuw <aagje.de.leeuw@student.ehb.be>
2024-11-13 22:14:04 +0100

    Milestone2-GenerativeDesign-AagjeDeLeeuw-AddedBubbles

--- generativeDesign.js
+++ generativeDesign.js
@@ -6,26 +6,28 @@ import * as Utils from "./utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
+drawBubbles(250);
 drawRainbowCircles(25);
 drawRainbowCircles(25, width, height);
 
 function drawRainbowCircles(numberOfCircles, x = 0, y = 0) {
     let maxRadius = 750;
-
     for (let i = 0; i < numberOfCircles; i++) {
         // Red is 0 and violet is 270
         let hue = (i * 320) / numberOfCircles;
         let radius = maxRadius - (i * maxRadius / numberOfCircles);
-
         context.lineWidth = 25;
         context.strokeStyle = Utils.hsl(hue, 100, 50);
         Utils.strokeCircle(x, y, radius);
     }
 }
 
-drawBubbles();
-
-function drawBubbles() {
-    context.fillStyle = "blue"
-    Utils.fillCircle(width/2, height/2, 50)
+function drawBubbles(bubbles) {
+    for (let i = 0; i < bubbles; i++) {
+        let hue = Utils.randomNumber(200, 260);
+        context.fillStyle = Utils.hsla(hue, 100, 50, 2 / 3 * 100);
+        let x = Utils.randomNumber(0, width);
+        let y = Utils.randomNumber(0, height);
+        Utils.fillCircle(x, y, Utils.randomNumber(10, 70));
+    }
 }
