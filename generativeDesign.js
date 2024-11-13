bdc41e9a26201ba668982f6f059a5cbb643fff81
AagjeDeLeeuw <aagje.de.leeuw@student.ehb.be>
2024-11-13 20:28:29 +0100

    Milestone2-GenerativeDesign-AagjeDeLeeuw-drawRainbowCircles

--- generativeDesign.js
+++ generativeDesign.js
@@ -5,3 +5,16 @@ import * as Utils from "./utils.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
+
+drawRainbowCircles(25);
+
+function drawRainbowCircles(numberOfCircles) {
+    for (let i = 0; i < numberOfCircles; i++) {
+        let radius = 750 / numberOfCircles
+        let hue = 300 / numberOfCircles;
+        context.strokeStyle = Utils.hsl(i * hue, 100, 50);
+        context.lineWidth = 25;
+        Utils.strokeCircle(0, 0, 750 - i * radius);
+        console.log("Drawing circle " + i);
+    }
+}
