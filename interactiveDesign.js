e2fae1aa446e6deef00c8386cedd54057b8f019f
AagjeDeLeeuw <aagje.de.leeuw@student.ehb.be>
2024-12-18 15:27:07 +0100

    Deadline-InteractiveDesign-AagjeDeLeeuw-SuccesfullyAnimatedRainbows

--- interactiveDesign.js
+++ interactiveDesign.js
@@ -7,89 +7,14 @@ let width = context.canvas.width;
 let height = context.canvas.height;
 
 let index = 0;
+
 let numberOfCircles = 25;
 let lineWidth = 25;
 let maxRadius = Math.min((width * 3) / 4, (height * 3) / 4);
+let defaultOpacity = 50;
 
 let bubbles = [];
-let circles = [];
-
-drawBubbles(250);
-
-function drawBubbles(numberOfBubbles) {
-	for (let i = 0; i < numberOfBubbles; i++) {
-		makeBubble();
-	}
-	for (let i = 0; i < bubbles.length; i++) {
-		let bubble = bubbles[i];
-		context.fillStyle = Utils.hsla(bubble.color, 100, 50, (2 / 3) * 100);
-		Utils.fillCircle(bubble.x, bubble.y, bubble.radius);
-	}
-}
-
-function makeBubble() {
-	bubbles.push({
-		x: Utils.randomNumber(0, width),
-		y: Utils.randomNumber(0, height),
-		radius: Utils.randomNumber(10, 70),
-		color: Utils.randomNumber(190, 240),
-	});
-}
-
-rainbowCircles(numberOfCircles);
-rainbowCircles(numberOfCircles, width, height);
-
-function rainbowCircles(numberOfCircles, x = 0, y = 0) {
-	makeRainbowCircle(numberOfCircles, x, y);
-	drawRainbowCircles(numberOfCircles, x, y);
-}
-
-function makeRainbowCircle(numberOfCircles, x = 0, y = 0) {
-	context.fillStyle = "white";
-	Utils.fillCircle(x, y, maxRadius + lineWidth / 2);
-
-	for (let i = 0; i < numberOfCircles; i++) {
-		makeCircle(i, numberOfCircles);
-	}
-}
-
-function drawRainbowCircles(numberOfCircles, x = 0, y = 0) {
-	for (let i = 0; i < circles.length; i++) {
-		let circle = circles[i];
-		context.lineWidth = lineWidth;
-		context.strokeStyle = Utils.hsla(circle.color, 100, 50, circle.opacity);
-		Utils.strokeCircle(x, y, circle.radius);
-	}
-}
-
-function makeCircle(i, numberOfCircles) {
-	circles.push({
-		// Red is 0 and violet is 270
-		color: (i * 320) / numberOfCircles,
-		opacity: 50,
-		radius: maxRadius - (i * maxRadius) / numberOfCircles,
-	});
-}
-
-animate();
-
-function animate() {
-	context.clearRect(0, 0, width, height);
-
-	let current = index % circles.length;
-	let previous = (index - 1 + circles.length) % circles.length;
-
-	circles[current].opacity = 100;
-	circles[previous].opacity = 50;
-
-	drawBubbles();
-
-	rainbowCircles();
-	rainbowCircles();
-
-	index++;
-	requestAnimationFrame(animate);
-}
+let rainbows = [];
 
 signature(50);
 
@@ -103,4 +28,95 @@ function signature(size) {
 	context.fillRect(width - 125, height - 125, size, size);
 	context.fillRect(width - 275, height - 75, 2 * size, size);
 	context.fillRect(width - 125, height - 75, 2 * size, size);
+}
+
+makeBubbles(250);
+
+function makeBubbles(numberOfBubbles) {
+	for (let i = 0; i < numberOfBubbles; i++) {
+		makeBubble();
+	}
+}
+
+function makeBubble() {
+	bubbles.push({
+		x: Utils.randomNumber(0, width),
+		y: Utils.randomNumber(0, height),
+		radius: Utils.randomNumber(10, 70),
+		color: Utils.randomNumber(190, 240),
+	});
+}
+
+function drawBubbles() {
+	for (let i = 0; i < bubbles.length; i++) {
+		let bubble = bubbles[i];
+		context.fillStyle = Utils.hsla(bubble.color, 100, 50, (2 / 3) * 100);
+		Utils.fillCircle(bubble.x, bubble.y, bubble.radius);
+	}
+}
+
+makeRainbows();
+
+function makeRainbows() {
+	rainbows.push({
+		x: 0,
+		y: 0,
+		circles: [],
+	});
+	rainbows.push({
+		x: width,
+		y: height,
+		circles: [],
+	});
+
+	for (let i = 0; i < rainbows.length; i++) {
+		for (let j = numberOfCircles; j >= 0; j--) {
+			rainbows[i].circles.push(makeCircle(rainbows[i].x, rainbows[i].y, j));
+		}
+	}
+}
+
+function makeCircle(x, y, i) {
+	return {
+		x: x,
+		y: y,
+		// Red is 0 and violet is 270
+		color: (i * 320) / numberOfCircles,
+		opacity: defaultOpacity,
+		radius: maxRadius - (i * maxRadius) / numberOfCircles,
+	};
+}
+
+function drawRainbowCircles() {
+	for (let i = 0; i < rainbows.length; i++) {
+		context.fillStyle = "white";
+		Utils.fillCircle(rainbows[i].x, rainbows[i].y, maxRadius + lineWidth / 2);
+		let rainbow = rainbows[i];
+		for (let j = 0; j < rainbow.circles.length; j++) {
+			let circle = rainbow.circles[j];
+			context.lineWidth = lineWidth;
+			context.strokeStyle = Utils.hsla(circle.color, 100, 50, circle.opacity);
+			Utils.strokeCircle(circle.x, circle.y, circle.radius);
+		}
+	}
+}
+
+animate();
+
+function animate() {
+	context.clearRect(0, 0, width, height);
+
+	let current = index % numberOfCircles;
+	let previous = (index - 1 + numberOfCircles) % numberOfCircles;
+
+	for (let i = 0; i < rainbows.length; i++) {
+		rainbows[i].circles[current].opacity = 100;
+		rainbows[i].circles[previous].opacity = defaultOpacity;
+	}
+
+	drawBubbles();
+	drawRainbowCircles();
+    signature(50);
+	index++;
+	requestAnimationFrame(animate);
 }
